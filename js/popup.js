2ecca8f7097cc2e4893f7a79cf224bbed8cef3fd
phong25t25t <phong25t25t@gmail.com>
2023-09-07 21:45:25 +0700

    Update popup html letter

--- js/popup.js
+++ js/popup.js
@@ -30,6 +30,7 @@ function onClicked(element) {
         element.classList.add("is-choose");
         resetClass("is-show");
         setClassByValue(element.dataset.valueTarget);
+        selectLang = element.dataset.valueTarget;
     });
 }
 
@@ -40,3 +41,22 @@ function resetClass(className) {
 function setClassByValue(value) {
     document.getElementById('img-' + value).classList.add("is-show");
 }
+
+// Show detail letter when click on letter tag
+const boxes = document.querySelectorAll('.module-modal');
+const overplay = document.getElementById('module-overlay')
+boxes.forEach(box => {
+    box.addEventListener('click', function handleClick(event) {
+        let letter = this.dataset.letter;
+        let folder = (selectLang === 'hiragana') ? 'hira' : 'kana';
+        overplay.innerHTML = `<figure class="figure-detail"><img src="/images/detail/${folder}/${letter}.png" alt=""></figure>`
+        overplay.classList.add("is-show");
+    });
+});
+// Hide overplay show detail of letter
+overplay.addEventListener('click', function handleClick(event) {
+    const figure_detail = event.target.closest('.figure-detail');
+    if (!figure_detail) {
+        overplay.classList.remove("is-show");
+    }
+});
